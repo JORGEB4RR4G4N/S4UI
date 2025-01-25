d13a0288ae353aecc8412ad18670641f966764f2
Jorge Barragan <developbarragan@gmail.com>
2025-01-24 22:07:33 -0600

    Feat Se anexo los projectos de Biblioteca de razor Feat se anexo biblioteca de clases para el cliente Feat se configuro las versiones de net8 Se hizo referencias a los proyectos de cliente y componetes a SA.UI

---
+++ S4.UI/Pages/PatrocinadoresComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace S4.UI.Pages
+{
+    public partial class PatrocinadoresComponent
+    {
+        public PatrocinadoresComponent()
+        {
+
+        }
+    }
+}
